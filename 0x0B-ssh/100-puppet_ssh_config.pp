a8fd6f7eb629a605b5e1ab59d1d41bfb03f56ed5
Linda-Njau <lindanjau21@.com>
2023-03-26 07:48:54 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+#modifying ssh configuration to refuse password
+include stdlib
+
+file_line { 'configure SSH':
+  ensure  => present,
+  path    => 'etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no\n     IdentityFile ~/.ssh/school',
+  replace => true,
+}
