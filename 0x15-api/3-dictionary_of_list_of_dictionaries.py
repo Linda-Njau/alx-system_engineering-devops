05c7c5f7ff9f8893faa42551f1944961ad7280c1
Linda-Njau <lindanjau21@.com>
2023-04-26 18:21:21 +0300

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,15 +6,14 @@ on all employees based on id to JSON format
 import json
 import requests
 
-url = "https://jsonplaceholder.typicode.com/"
-employees = requests.get(url + "users").json()
+employees = requests.get("https://jsonplaceholder.typicode.com/users").json()
 with open("todo_all_employees.json", "w") as jsonfile:
     json.dump({
         employee.get("id"): [{
             "task": task.get("title"),
             "completed": task.get("completed"),
-            "username": employee.get("username")s
-        } for task in requests.get(url + "todos",
+            "username": employee.get("username"),
+        } for task in requests.get("https://jsonplaceholder.typicode.com/todos",
                                    params={"userId":
                                            employee.get("id")}).json()]
         for employee in employees}, jsonfile)
