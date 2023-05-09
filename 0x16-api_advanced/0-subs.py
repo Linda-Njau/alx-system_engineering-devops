b2718dd17497ef700347ba14dd25f5ac9b7b9eac
Linda-Njau <lindanjau21@gmail.com>
2023-05-09 13:14:23 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that queries Reddit for info on subscribers of a subreddit"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """function to return subscriber info"""
+    url = "https://www.reddit.com/r{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:myserver:v5.11.0 (by /u/linda)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+This repository contains python scripts exploring advanced api concepts
